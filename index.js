a99b99f904720b9ec14809f8f91b7f7d1e4bc59c
Amber <amberpitts@gmail.com>
2022-12-21 08:18:45 -0700

    adding begining code

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {url} = require('inspector');
+
+//ask all the questions using the inquire toolbox/prompt
+
+//from there, we have thr gigantic string of text that we need to get onto the readme
+
